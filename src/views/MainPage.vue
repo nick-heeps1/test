35a09cf0320d0d748b4856a8b345f2acf2375fd7
nick <s223167228@deakin.edu.au>
2023-09-13 21:50:23 +1000

    detailing about me section

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -10,7 +10,7 @@
                     <div class ="link">
                       <a href="https://github.com/nick-heeps1">
                         <img src="../assets/github.png" alt="">
-                        <h2>github</h2>
+                        <h2>GitHub</h2>
                       </a>
                     </div>
                     <div class ="link">
@@ -22,7 +22,7 @@
                     <div class ="link">
                       <a href="https://www.youtube.com/channel/UCfIYBNfilN8YQbqf8BroyiQ">
                         <img src="../assets/youtube.png" alt="">
-                        <h2>youtube</h2>
+                        <h2>YouTube</h2>
 
                       </a>
                     </div>
@@ -31,19 +31,24 @@
         <div class="container-two">
             <div class="about-me">
                 <h2>About Me</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                <p>Hey my name is Nick, my Deakin student number is s223167228 and my email address is s223167228@deakin.edu.au. I am a new student here at Deakin and I’m currently studying for a bachelor’s degree in information technology. 
+                    I live at Deakin residence and have 3 days a week of contact. This is my second year at university as I had originally started off at RMIT but had decided to transfer as Deakin was more appealing to me. 
+                    Before moving to Melbourne, I used to live on a farm in a small town near the border called Barnawartha. 
+                    <br>
+                    I have a few different hobbies, but my main ones are coding, building PC’s and going to gym. Since first beginning university, I have created a few projects including writing a choose your own adventure game in python, 
+                    creating a basic bartending simulator in unreal engine, writing some basic websites, designing prototypes for apps and coding basic databases. 
+                    This year I have been mainly working with HTML where I have created webpages that display how people can interact in different ways and also displaying the many different ways that java can be utilized. 
+                    I have also been working with SQL and creating databases which then involves adding, altering, and removing sets of data. 
                 </p>
             </div>
             <div class="technology-section">
-                <h1>technologies studied</h1>
+                <h1>Technologies Studied</h1>
                 <div class="logos">
                     <img src="../assets/css-logo.png" alt="Css logo">
                     <img src="../assets/html-logo.png" alt="html logo">
                     <img src="../assets/JavaScript-Logo.png" alt="Javascript logo">
                     <img src="../assets/Python-logo.png" alt="python logo">
+                    <img src="../assets/SQL-logo.png" alt="SQL logo">
                 </div>
             </div>
             <div class="button-container">
@@ -80,7 +85,7 @@ export default {
     }
 
     .container-one{
-        background-color: #68fae0;
+        background-color: #202151;
         padding: 2.5%;
         width: 30%;
         border-radius: 25px;
@@ -105,6 +110,7 @@ export default {
         background-color: #5a6866;
         margin: 2.5%;
         width: 70%;
+        border-radius: 25px;
     }
 
     .about-me{
@@ -132,8 +138,17 @@ export default {
     }
     .projects-button a{
         text-decoration: none;
-        color: rgb(100, 100, 100);
-
     }
     
+    h2{
+        color: rgb(255, 255, 255);
+    }
+
+    p{
+        color: rgb(255, 255, 255);
+    }
+
+    h3{
+        color: rgb(255, 255, 255);
+    }
 </style>
