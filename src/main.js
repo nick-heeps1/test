eed5047aaebbf47c10f415a6bc3276e99c5e6b79
nick-heeps1 <s223167228@deakin.edu.au>
2023-09-20 12:40:37 +1000

    .

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './Components/App.vue'
+import App from './components/App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
